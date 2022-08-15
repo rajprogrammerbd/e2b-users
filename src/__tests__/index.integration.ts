512d4c938f921c9579c7fbae355f76d4e1e00907
Raj Dutta <rd2249619@gmail.com>
2022-08-16 01:54:56 +0600

    fix the test

--- src/__tests__/index.integration.ts
+++ src/__tests__/index.integration.ts
@@ -17,23 +17,3 @@ test('GET - / - Success to retrieve homepage endpoint', async () => {
     expect(res.statusCode).toBe(200);
     expect(res.body).toEqual({ message: 'Hello' });
 });
-
-test('POST - / - Send Mail fail because of missing req.body', async () => {
-    const res = await request(appPort).post('/api/email/send');
-
-    // Check when user doesn't pass any request body.
-    expect(res.statusCode).toBe(404);
-    expect(res.body).toEqual({ message: 'Request body is required' });
-});
-
-test('POST - / -  Send mail successfully', async () => {
-    // That the function sends the email.
-    const test2 = await request(appPort).post('/api/email/send').send({
-        to: "rd2249619@gmail.com",
-        title: "Send email for automation testing",
-        html: "<b>Test has done successfully.</b>"
-    });
-
-    expect(test2.statusCode).toBe(200);
-    expect(test2.body).toEqual({ message: 'Email has been successfully send to rd2249619@gmail.com' });
-});

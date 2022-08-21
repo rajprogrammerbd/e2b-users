3fdae55e1d91398a59a0fdd22410fc1e395b05de
Raj Dutta <rd2249619@gmail.com>
2022-08-21 21:13:11 +0600

    remove unused import

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,3 @@
-import mongoose from 'mongoose';
-
 export const createUserText = "Welcome to E2B Translator";
 export const createUserHtml = "<b>Thank you for created an account in <a href='https://e2b-translator-be.herokuapp.com/'>E2B Translator</a></b>";
 
